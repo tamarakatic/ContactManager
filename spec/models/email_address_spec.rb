071c82283893228a62a6afaaccd7d7d20a25013f
tamarakatic <tamarakatic@gmail.com>
2017-07-30 22:49:26 +0200

    Refactoring

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email_address) do
+    EmailAddress.new(address: 'Njegoseva 20', person_id: 1)
+  end
+
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
+
+  it 'is invalid with invalid address' do
+    email_address.address = nil
+    expect(email_address).not_to be_valid
+  end
+
+  it 'is invalid unless it has a person_id' do
+    email_address.person_id = nil
+    expect(email_address).not_to be_valid
+  end
 end
